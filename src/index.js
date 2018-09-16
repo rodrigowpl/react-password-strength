e019aa6961bc9f8417a405e00affd101c03de8e7
gtkatakura <gt.katakura@gmail.com>
2018-09-16 02:00:55 -0300

    perf: avoid rerender when score is the same

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import { number, string, object } from 'prop-types'
 import zxcvbn from 'zxcvbn'
 import styled from 'styled-components'
@@ -6,7 +6,7 @@ import styled from 'styled-components'
 import Meters from './meters'
 import { getStrengthColor, getStrengthText } from './utils/functions'
 
-const PasswordStrengthStyled = styled.div`
+const StrengthStyled = styled.div`
   width: 300px;
   display: flex;
   flex-direction: column;
@@ -24,29 +24,43 @@ const StrengthLabelStyled = styled.span`
   color: ${props => props.color};
 `
 
-const PasswordStrength = ({ width = 300, value, strengthColors, strengthTexts }) => {
+class Strength extends PureComponent {
+  render() {
+    const { width = 300, showLabel, score, strengthColors, strengthTexts } = this.props
+    const maxMeters = 4
+    const meterWidth = width / maxMeters
+    const strengthColor = getStrengthColor(score, strengthColors)
+    const strengthText = getStrengthText(score, strengthTexts)
+    const meterValue = score === 0 ? maxMeters : score
+
+    return (
+      <StrengthStyled>
+        <div className='meters'>
+          <Meters
+            meterValue={meterValue}
+            meterWidth={meterWidth}
+            backgroundColor={strengthColor}
+          />
+        </div>
+        {showLabel &&
+          <StrengthLabelStyled color={strengthColor}>
+            {strengthText}
+          </StrengthLabelStyled>
+        }
+      </StrengthStyled>
+    )
+  }
+}
+
+const PasswordStrength = ({ value, ...props }) => {
   const { score } = zxcvbn(value)
-  const maxMeters = 4
-  const meterWidth = width / maxMeters
-  const strengthColor = getStrengthColor(score, strengthColors)
-  const strengthText = getStrengthText(score, strengthTexts)
-  const meterValue = score === 0 ? maxMeters : score
 
   return (
-    <PasswordStrengthStyled>
-      <div className='meters'>
-        <Meters
-          meterValue={meterValue}
-          meterWidth={meterWidth}
-          backgroundColor={strengthColor}
-        />
-      </div>
-      {value &&
-        <StrengthLabelStyled color={strengthColor}>
-          {strengthText}
-        </StrengthLabelStyled>
-      }
-    </PasswordStrengthStyled>
+    <Strength
+      score={score}
+      showLabel={!!value}
+      {...props}
+    />
   )
 }
 
